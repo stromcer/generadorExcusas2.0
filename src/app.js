9277f947c9b4d43c9e502851206ea433c14d7025
Daniel Manjabacas <dannymm12@hotmail.com>
2023-01-31 23:45:47 +0000

    Depurado codigo del generador

--- src/app.js
+++ src/app.js
@@ -38,33 +38,33 @@ window.onload = function() {
 
   function excusaFinal() {
     let quien = [
-      "El perro ",
-      "Mi abuela ",
-      "Un mono de tres cabezas ",
-      "Elvis presley resucitado ",
-      "Uno de los monty python ",
-      "Ganondorf "
+      "El perro",
+      "Mi abuela",
+      "Un mono de tres cabezas",
+      "Elvis presley resucitado",
+      "Uno de los monty python",
+      "Ganondorf",
     ];
     let que = [
-      "lo ha destrozado ",
-      "lo usó como ingrediente para pizzas ",
-      "lo utilizo como un bolo ",
-      "lo quemo mientras probaba un traje ignifugo ",
-      "lo chasqueo como si fuera thanos y desaparecio "
+      "lo ha destrozado",
+      "lo usó como ingrediente para pizzas",
+      "lo utilizo como un bolo",
+      "lo quemo mientras probaba un traje ignifugo",
+      "lo chasqueo como si fuera thanos y desaparecio",
     ];
     let cuando = [
-      "mientras cenaba.",
-      "cuando estaba jugando.",
-      "a media dungeon mitica.",
-      "cuando construia un reactor nuclear.",
-      "mientras estaba en el pasado salvando hyrule"
+      "mientras cenaba",
+      "cuando estaba jugando",
+      "a media dungeon mitica",
+      "cuando construia un reactor nuclear",
+      "mientras estaba en el pasado salvando hyrule",
     ];
 
     let quienIndex = Math.floor(Math.random() * quien.length);
     let queIndex = Math.floor(Math.random() * que.length);
     let cuandoIndex = Math.floor(Math.random() * cuando.length);
 
-    let excusaFinal = quien[quienIndex] + que[queIndex] + cuando[cuandoIndex];
+    let excusaFinal = `${quien[quienIndex]} ${que[queIndex]} ${cuando[cuandoIndex]}.`;
 
     let quienImagen = [
       "https://i.imgur.com/zXGdRb3.jpg",
@@ -72,7 +72,7 @@ window.onload = function() {
       "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9be23d57-8824-450b-88db-0ae58ef5934e/d1p7knt-55f9999a-e56f-45be-8c38-a02be797a7d3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzliZTIzZDU3LTg4MjQtNDUwYi04OGRiLTBhZTU4ZWY1OTM0ZVwvZDFwN2tudC01NWY5OTk5YS1lNTZmLTQ1YmUtOGMzOC1hMDJiZTc5N2E3ZDMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.3CqnlobhYEZqr7dZ4TCTZYIiXOxB9fEorD8FvlQ2o-A",
       "https://ichef.bbci.co.uk/news/976/cpsprodpb/4D8E/production/_95845891_img_4419.jpg.webp",
       "https://phantom-elmundo.unidadeditorial.es/8a38e700cc89eaa02490fe89603df7e4/resize/473/f/webp/assets/multimedia/imagenes/2020/01/22/15797007276268.gif",
-      "https://www.nintenderos.com/wp-content/uploads/2017/02/71455290.jpg"
+      "https://www.nintenderos.com/wp-content/uploads/2017/02/71455290.jpg",
     ];
 
     let mostrarQuienImagen = `<img src="${quienImagen[quienIndex]}"  class="comicImagen "/>`;
@@ -82,7 +82,7 @@ window.onload = function() {
       "https://www.hogarmania.com/archivos/202102/como-hacer-pizza-casera-masa-recetas-668x400x80xX-1.jpg",
       "https://hazrevista.org/wp-content/uploads/2014/01/bolos.jpg",
       "https://m.media-amazon.com/images/I/81C77n2RbCL._AC_SL1500_.jpg",
-      "https://www.latercera.com/resizer/fa4emg5E1Jm5etLjcYWmm4EDhPQ=/900x600/filters:focal(1020x228:1030x218)/cloudfront-us-east-1.images.arcpublishing.com/copesa/FE43BZR7FND5DEHB4DNQZQOEWI.jpg"
+      "https://www.latercera.com/resizer/fa4emg5E1Jm5etLjcYWmm4EDhPQ=/900x600/filters:focal(1020x228:1030x218)/cloudfront-us-east-1.images.arcpublishing.com/copesa/FE43BZR7FND5DEHB4DNQZQOEWI.jpg",
     ];
 
     let mostrarQueImagen = `<img src="${queImagen[queIndex]}" class="comicImagen "/>`;
@@ -92,7 +92,7 @@ window.onload = function() {
       "https://previews.123rf.com/images/graphicbee/graphicbee1611/graphicbee161100178/66571623-felices-los-ni%C3%B1os-jugando-bajo-el-sol.jpg",
       "https://cdnb.20m.es/videojuegos/files/playingwow.jpg",
       "https://www.theneweconomy.com/wp-content/uploads/2016/03/China-nuclear-reactor-construction.jpg",
-      "https://www.wikihow.com/images/thumb/9/9c/Save-in-The-Legend-of-Zelda-Games-Step-14.jpg/aid5377845-v4-728px-Save-in-The-Legend-of-Zelda-Games-Step-14.jpg.webp"
+      "https://www.wikihow.com/images/thumb/9/9c/Save-in-The-Legend-of-Zelda-Games-Step-14.jpg/aid5377845-v4-728px-Save-in-The-Legend-of-Zelda-Games-Step-14.jpg.webp",
     ];
     let mostrarCuandoImagen = `<img src="${cuandoImagen[cuandoIndex]}" class="comicImagen"/>`;
 
@@ -100,7 +100,7 @@ window.onload = function() {
       excusaFinal,
       mostrarQuienImagen,
       mostrarQueImagen,
-      mostrarCuandoImagen
+      mostrarCuandoImagen,
     ];
   }
 };
