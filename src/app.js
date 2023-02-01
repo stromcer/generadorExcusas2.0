e099b5e975dbc9dd88af8894c82e5d6902ab3536
Daniel Manjabacas <dannymm12@hotmail.com>
2023-02-01 00:07:41 +0000

    No se si he cambiado algo pero git status dice que si

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ window.onload = function() {
       "Un velocirraptor en moto lanzando petalos",
       "Jesucristo-colega",
       "La rana Pepe",
-      "Cristian",
+      "Cristian"
     ];
     let que = [
       "lo ha destrozado",
@@ -55,14 +55,14 @@ window.onload = function() {
       "lo utilizo como un bolo",
       "se quemo mientras probaba un traje ignifugo",
       "chasqueo con el guante de thanos y desaparecio",
-      "estaba usandolo como un Frisbee",
+      "estaba usandolo como un Frisbee"
     ];
     let cuando = [
       "mientras cenaba",
       "cuando estaba jugando",
       "a media dungeon mitica",
       "cuando construia un reactor nuclear",
-      "mientras estaba en el pasado salvando hyrule",
+      "mientras estaba en el pasado salvando hyrule"
     ];
 
     let quienIndex = Math.floor(Math.random() * quien.length);
@@ -81,7 +81,7 @@ window.onload = function() {
       "https://scontent.fmad7-1.fna.fbcdn.net/v/t1.18169-9/375267_296045777095916_1749474767_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=9267fe&_nc_ohc=juTNXCce2QoAX86lM3E&_nc_ht=scontent.fmad7-1.fna&oh=00_AfBDgRHd9kuFlPXBNpX4PlruHQOQgQbwBbmGpnZUpaaNug&oe=64011AF2",
       "https://s1.eestatic.com/2017/04/15/actualidad/actualidad_208739511_32684409_1706x960.jpg",
       "https://image.cnbcfm.com/api/v1/image/103978904-The_meme_formerly_known_as_Kuk_1.png",
-      "https://ca.slack-edge.com/T0BFXMWMV-U03KR679V5Y-ab2cbae00711-512",
+      "https://ca.slack-edge.com/T0BFXMWMV-U03KR679V5Y-ab2cbae00711-512"
     ];
 
     let mostrarQuienImagen = `<img src="${quienImagen[quienIndex]}"  class="comicImagen "/>`;
@@ -92,7 +92,7 @@ window.onload = function() {
       "https://hazrevista.org/wp-content/uploads/2014/01/bolos.jpg",
       "https://m.media-amazon.com/images/I/81C77n2RbCL._AC_SL1500_.jpg",
       "https://www.latercera.com/resizer/fa4emg5E1Jm5etLjcYWmm4EDhPQ=/900x600/filters:focal(1020x228:1030x218)/cloudfront-us-east-1.images.arcpublishing.com/copesa/FE43BZR7FND5DEHB4DNQZQOEWI.jpg",
-      "https://sports-images.vice.com/images/articles/meta/2015/10/16/ultimate-el-deporte-ms-buena-onda-del-mundo-1445014923.png",
+      "https://sports-images.vice.com/images/articles/meta/2015/10/16/ultimate-el-deporte-ms-buena-onda-del-mundo-1445014923.png"
     ];
 
     let mostrarQueImagen = `<img src="${queImagen[queIndex]}" class="comicImagen "/>`;
@@ -102,7 +102,7 @@ window.onload = function() {
       "https://previews.123rf.com/images/graphicbee/graphicbee1611/graphicbee161100178/66571623-felices-los-ni%C3%B1os-jugando-bajo-el-sol.jpg",
       "https://cdnb.20m.es/videojuegos/files/playingwow.jpg",
       "https://www.theneweconomy.com/wp-content/uploads/2016/03/China-nuclear-reactor-construction.jpg",
-      "https://www.wikihow.com/images/thumb/9/9c/Save-in-The-Legend-of-Zelda-Games-Step-14.jpg/aid5377845-v4-728px-Save-in-The-Legend-of-Zelda-Games-Step-14.jpg.webp",
+      "https://www.wikihow.com/images/thumb/9/9c/Save-in-The-Legend-of-Zelda-Games-Step-14.jpg/aid5377845-v4-728px-Save-in-The-Legend-of-Zelda-Games-Step-14.jpg.webp"
     ];
     let mostrarCuandoImagen = `<img src="${cuandoImagen[cuandoIndex]}" class="comicImagen"/>`;
 
@@ -110,7 +110,7 @@ window.onload = function() {
       excusaFinal,
       mostrarQuienImagen,
       mostrarQueImagen,
-      mostrarCuandoImagen,
+      mostrarCuandoImagen
     ];
   }
 };
